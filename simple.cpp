7bca5f57cb8d166f139ccc93134be47bf14ac442
krynnlord <krynnlord@yahoo.com>
2016-04-02 17:36:30 -0500

    reformatted to c++

--- simple.cpp
+++ simple.cpp
@@ -1,12 +1,10 @@
-/* Working file for simple */
-
 #include <stdio.h>
 
-main()
+int main()
 {
     int a;
     printf("Hello World\n");
-    printf("how are old are you-->");`
+    printf("how are old are you-->");
     scanf("%d", &a);
     printf("Really? I am also %d\n",a);
 return 0;
