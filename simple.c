0badd797c9bbc44692dfb798ab499856476406a6
Richard Miller <richardmiller@Richards-MacBook-Pro.local>
2016-04-02 16:45:50 -0500

    try again

--- simple.c
+++ simple.c
@@ -7,7 +7,6 @@ main()
     printf("how are old are you-->");
     scanf("%d", &a);
     printf("Really? I am also %d\n",a);
-    printf("Intresting huh?\n");
-    printf("I just added a new line with Code\n");	    
+    printf("Intresting huh?\n");    
 return 0;
 }
