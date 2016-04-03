2e42e115de8d93cd6a51bf7f6db5fa8d01ddcc34
krynnlord <krynnlord@yahoo.com>
2016-04-02 22:40:14 -0500

    goodnight

--- simple.cpp
+++ simple.cpp
@@ -6,5 +6,6 @@ int main()
     printf("Welcome to the wonderful Calculator 10,000!\n");
     printf("I will calculate what ever commands you enter\n");
     printf("Enter 'q' to quit\n");
+ 
  return 0;
 }
