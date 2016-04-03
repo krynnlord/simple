0a9c3fd3980d370cc9ec6a2ae58fceb8b0a6df52
krynnlord <krynnlord@yahoo.com>
2016-04-02 21:10:23 -0500

    last edit tonight

--- simple.cpp
+++ simple.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     int a;
-    printf("Hello World\n");
-    printf("how are old are you-->");
-    scanf("%d", &a);
-    printf("Really? I am also %d\n",a);
+    printf("Welcome to the wonderful Calculator 10,000!\n");
+    printf("I will calculate what ever commands you enter\n");
+    printf("Enter 'q' to quit\n");
+    printf(": ");
 return 0;
 }
