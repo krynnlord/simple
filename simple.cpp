dfa66ae2cf6d0314c580a9130c10cb98de3b9001
krynnlord <krynnlord@yahoo.com>
2016-04-02 22:04:31 -0500

    Now i am using Sublime

--- simple.cpp
+++ simple.cpp
@@ -6,6 +6,5 @@ int main()
     printf("Welcome to the wonderful Calculator 10,000!\n");
     printf("I will calculate what ever commands you enter\n");
     printf("Enter 'q' to quit\n");
-    printf(": ");
-return 0;
+ return 0;
 }
