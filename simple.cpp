47c9cc994d935d6151324d7beeb179153007470c
Krynnlord <krynnlord@yahoo.com>
2016-04-04 08:48:12 -0500

    changed in windows
    don't worry about it

--- simple.cpp
+++ simple.cpp
@@ -4,6 +4,8 @@ int main()
 {
     int a;
     printf("Welcome to the wonderful Calculator 10,000!\n");
+    printf("Welcome to the wonderful Calculator 10,000!\n");
+    printf("Welcome to the wonderful Calculator 10,000!\n");
     printf("I will calculate what ever commands you enter\n");
     printf("Enter 'q' to quit\n");
 return 0;
