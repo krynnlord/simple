65b8dd4d52c9c802b74bb4f10316a129b2ca6fa9
krynnlord <krynnlord@yahoo.com>
2016-04-04 09:41:54 +0000

    changed

--- simple.cpp
+++ simple.cpp
@@ -4,8 +4,6 @@ int main()
 {
     int a;
     printf("Welcome to the wonderful Calculator 10,000!\n");
-    printf("Welcome to the wonderful Calculator 10,000!\n");
-    printf("Welcome to the wonderful Calculator 10,000!\n");
     printf("I will calculate what ever commands you enter\n");
     printf("Enter 'q' to quit\n");
 return 0;
